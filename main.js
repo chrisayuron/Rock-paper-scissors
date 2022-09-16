6bf7177dbc48dcee057da2130db2c5db836f925f
Ronald Rojas Newball <rrnewball@gmail.com>
2022-09-16 11:30:24 -0500

    add loop times and select user

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let times=+prompt("how many times would you like to play")
 let pcpoints=0
 let userpoints=0
+
+for (let i = 0; i < times; i++) {
+    let user = 0
+    while (user <= 0 || user > 3 || user=='Nan') {
+        user = +prompt("Choose a option \n 1. Piedra \n 2. Papel \n 3. Tijera")
+    }
+}
