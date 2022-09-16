87b9f51b110584847711b7ca52b3ff6c96312661
Ronald Rojas Newball <rrnewball@gmail.com>
2022-09-16 11:35:16 -0500

    add function select and choose

--- main.js
+++ main.js
@@ -7,4 +7,24 @@ for (let i = 0; i < times; i++) {
     while (user <= 0 || user > 3 || user=='Nan') {
         user = +prompt("Choose a option \n 1. Piedra \n 2. Papel \n 3. Tijera")
     }
+
+    user = choose(user)
+    let pc = select()
+
+    function select() {
+        let value = Math.floor(Math.random() * 3 + 1)
+        return choose(value)
+    }
+
+    function choose(value) {
+        if (value == 1) return "Stone"
+        else if (value == 2) return "Paper"
+        else if (value == 3) return "Scissor"
+    }
+
+    if (user == pc) {
+        alert(`tie User: ${user} and PC: ${pc}`)
+        
+    }
+
 }
